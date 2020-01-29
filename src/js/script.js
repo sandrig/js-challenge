067938636d3635748b1ec16fed911cd32e56a93b
sandrig <sandrig.dev@gmail.com>
2020-01-29 20:26:39 +0300

    feat: add solution for challenge

---
+++ src/js/script.js
@@ -0,0 +1,59 @@
+const day = d => moment(d, 'dddd').format('ddd')
+
+const decimal = time => moment().startOf('d').add(time, 'h')
+
+const interval = ({start, end}) => {
+  const f = 'h:mm A'
+  return `${decimal(start).format(f)} - ${decimal(end).format(f)}`
+}
+
+const toHTML = item => `<li>${item}</li>`
+
+const append = (html, index) => document.querySelector(`#box${index + 1} ul`).innerHTML = html
+
+const filter = item => !!item
+
+function format({time, names}) {
+  if (names.length === 1) {
+    return `${day(names[0])}: ${time}`
+  } else {
+    return `${day(names[0])} - ${day(names[names.length - 1])}: ${time}`
+  }
+}
+
+function toIntervals({days, order}) {
+  let key = 0
+  let prev
+
+  return order.reduce((group, day) => {
+    if (!days[day]) {
+      key++
+      return group
+    }
+
+    const time = interval(days[day])
+
+    if (prev !== time) {
+      key++
+      prev = time
+    }
+
+    if (!group[key]) {
+      group[key] = {names: [], time}
+    }
+
+    group[key].names.push(day)
+
+    return group
+  }, [])
+}
+
+const convert = src => toIntervals(src)
+  .filter(filter)
+  .map(format)
+  .map(toHTML)
+  .join(' ');
+
+[source, source2, source3]
+  .map(convert)
+  .forEach(append)
